335498c7ad1e60c3ec7dcb04d20f6160982f1a18
Junior Miranda <jr.miranda@outlook.com>
2020-06-12 04:34:20 -0300

    refactor: change RemoteAuthentication to use faker

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,9 +1,10 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client';
+import faker from 'faker';
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
 
-  async post(url: string): Promise<void> {
+  async post(url: string = faker.internet.url()): Promise<void> {
     this.url = url;
     return Promise.resolve();
   }
